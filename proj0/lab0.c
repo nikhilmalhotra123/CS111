e74725ccb10015f4d0097571c00162d372c8b50d
Nikhil Malhotra <nikhil.ma12@gmail.com>
2020-03-31 11:22:28 -0700

    Complete till step 4

---
+++ proj0/lab0.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+void signal_handler() {
+    signal(SIGSEGV, SIG_DFL);
+    fprintf(stderr, "Segmentation fault due to segfault flag: %s\n", strerror(errno));
+    exit(4);
+}
+
+int main(int argc, char **argv) {
+  int c;
+  static int seg_flag = 0;
+  static int catch_flag = 0;
+  while (1) {
+    static struct option long_options[] = {
+        {"input_file",  required_argument,  0,  'i'},
+        {"output_file", required_argument,  0,  'o'},
+        {"segfault",    no_argument,        &seg_flag,  1},
+        {"catch",       no_argument,        &catch_flag,  1},
+        {0, 0, 0, 0}
+    };
+    int option_index = 0;
+    c = getopt_long (argc, argv, "i:o:", long_options, &option_index);
+    if (c == -1)
+        break;
+
+    int input;
+    int output;
+    switch (c) {
+      case 0:
+        break;
+      case 'i':
+        input = open(optarg, O_RDONLY);
+        if (input >= 0) {
+          close(0);
+          dup(input);
+          close(input);
+        }
+        else {
+          fprintf(stderr, "%s\n", strerror(errno));
+          exit(2);
+        }
+        break;
+      case 'o':
+        output = open(optarg, O_WRONLY | O_CREAT, 0777);
+        if (output >= 0) {
+          close(1);
+          dup(output);
+          close(output);
+        }
+        else {
+          fprintf(stderr, "%s\n", strerror(errno));
+          exit(3);
+        }
+        break;
+      default:
+        fprintf(stderr, "Unrecognized argument\n");
+        fprintf(stderr, "--input_file=<filename> File to replace standard input\n");
+        fprintf(stderr, "--output_file=<filename> File to replace standard output\n");
+        fprintf(stderr, "--segfault Force a segmentation fault\n");
+        fprintf(stderr, "--catch Catch a forced segmentation fault\n");
+        exit(1);
+      }
+  }
+  if (seg_flag) {
+    if (catch_flag) {
+      signal(SIGSEGV, signal_handler);
+    }
+    char* thing = NULL;
+    printf("%c", *thing);
+  }
+  char buf;
+  size_t size;
+  while ((size = read(0, &buf, 5)) > 0) {
+    write(1, &buf, size);
+  }
+}
