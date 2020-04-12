cdaf8a5e6e5dac5782aa33cf3c35ce1e8d4e1d3f
Nikhil Malhotra <nikhil.ma12@gmail.com>
2020-04-12 15:05:28 -0700

    begin working on pipes

--- 1a/lab1a.c
+++ 1a/lab1a.c
@@ -2,37 +2,127 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
 
 
 struct termios originalconfig, newconfig;
+char* exec_file;
+int pipeChild[2];
+int pipeParent[2];
 
-
-void restoreTerminal() {
-  tcsetattr(0, TCSANOW, &originalconfig);
+void restoreTerminal(void) {
+  tcsetattr(STDIN_FILENO, TCSANOW, &originalconfig);
 }
 
 void setTerminal() {
-  if (tcgetattr(0, &originalconfig) != 0 || tcgetattr(0, &newconfig) != 0) {
+  if (!isatty(STDIN_FILENO)) {
+    fprintf(stderr, "Not a terminal.\n");
+    exit(1);
+  }
+
+  if (tcgetattr(STDIN_FILENO, &originalconfig) != 0 || tcgetattr(STDIN_FILENO, &newconfig) != 0) {
     printf("Error getting terminal attributes");
     exit(1);
   }
   atexit(restoreTerminal);
 
-  newconfig.c_iflag = ISTRIP;
+  newconfig.c_iflag &= ISTRIP;
   newconfig.c_oflag = 0;
   newconfig.c_lflag = 0;
 
-  tcsetattr(0, TCSANOW, &newconfig);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newconfig);
+}
+
+void runChild() {
+  close(pipeChild[1]); //close child output
+  close(pipeParent[0]); //close input from parent
+
+  execvp(exec_file, NULL);
+}
+
+void runParent() {
+  close(pipeChild[0]); //close input from child
+  close(pipeParent[1]); //close parent output
+
+}
+
+void copy() {
+  // Copy
+  char buf[256];
+  char crlf[2] = {'\015', '\012'};
+  int size;
+  while ((size = read(STDIN_FILENO, &buf, 256)) > 0) {
+    for(int i = 0; i < size; i++) {
+      switch(buf[i]) {
+        case '\004':
+          exit(0);
+          break;
+        case '\012':
+        case '\015':
+          write(1, &crlf, 2);
+          break;
+        default:
+          write(1, &buf, size);
+          break;
+      }
+    }
+  }
 }
 
 int main(int argc, char **argv) {
-  setTerminal();
+  //Process args
+  int c;
+  int shell_flag = 0;
 
-  char buf[128];
-  size_t size;
-  while ((size = read(0, &buf, sizeof(char)*128)) > 0) {
-    write(1, &buf, size*sizeof(char));
+  while(1) {
+    static struct option long_options[] = {
+        {"shell",  optional_argument,  0,  's'},
+        {0, 0, 0, 0}
+    };
+    int option_index = 0;
+    c = getopt_long(argc, argv, "s:", long_options, &option_index);
+    if (c == -1)
+        break;
+
+    switch(c) {
+      case 's':
+        shell_flag = 1;
+        exec_file = optarg;
+        fprintf(stderr, "shell\n");
+        fprintf(stderr, "%s\n", exec_file);
+        break;
+      default:
+        fprintf(stderr, "Unrecognized argument\n");
+        fprintf(stderr, "--shell=<program> Shell program\n");
+    }
   }
 
+  setTerminal();
+
+  if (shell_flag) {
+    if (pipe(pipeChild) != 0) {
+      fprintf(stderr, "%s\n", strerror(errno));
+      exit(1);
+    }
+    if (pipe(pipeParent) != 0) {
+      fprintf(stderr, "%s\n", strerror(errno));
+      exit(1);
+    }
+    int output = fork();
+    if (output < -1) {
+      fprintf(stderr, "%s\n", strerror(errno));
+      exit(1);
+    }
+    else if (output == 0) {
+      runChild();
+    }
+    else {
+      runParent();
+    }
+  }
+
+  copy();
   exit(0);
 }
