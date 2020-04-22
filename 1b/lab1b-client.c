93ee59551d59714ed38a5537ed0f5153409847d3
Nikhil Malhotra <nikhil.ma12@gmail.com>
2020-04-21 19:13:45 -0700

    ready to test

--- 1b/lab1b-client.c
+++ 1b/lab1b-client.c
@@ -16,24 +16,20 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <fcntl.h>
+#include <assert.h>
+#include "zlib.h"
 
 struct termios originalconfig, newconfig;
 int port;
 int sockfd;
+int logfile;
+int log_flag, compress_flag;
+z_stream from_input;
+z_stream from_server;
 
 void restoreTerminal(void) {
   tcsetattr(STDIN_FILENO, TCSANOW, &originalconfig);
-  if (shell_flag) {
-    int status;
-    if (waitpid(pid, &status, 0) != pid) {
-      fprintf(stderr, "Waitpid error: %s\n", strerror(errno));
-      exit(1);
-    }
-    if (WIFEXITED(status)) {
-      fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\n", WTERMSIG(status), WEXITSTATUS(status));
-      exit(0);
-    }
-  }
 }
 
 void setTerminal() {
@@ -58,7 +54,6 @@ void setTerminal() {
 void connectToServer() {
   struct sockaddr_in serv_addr;
   struct hostent *server;
-  char buffer[256];
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
@@ -81,13 +76,168 @@ void connectToServer() {
   //Succesfully connected to server
 }
 
+void setupCompression() {
+  from_input.zalloc = Z_NULL;
+  from_input.zfree = Z_NULL;
+  from_input.opaque = Z_NULL;
+  int ret = deflateInit(&from_input, Z_DEFAULT_COMPRESSION);
+  if (ret < 0) {
+    fprintf(stderr, "Failed compression init\n");
+    exit(1);
+  }
+
+  from_server.zalloc = Z_NULL;
+  from_server.zfree = Z_NULL;
+  from_server.opaque = Z_NULL;
+  ret = inflateInit(&from_server);
+  if (ret < 0) {
+    fprintf(stderr, "Failed decompression init\n");
+    exit(1);
+  }
+}
+
+int compression(int bytes_to_compress, char* buffer, char* compressedBuffer, int compressedBufferSize) {
+  from_input.avail_in = bytes_to_compress;
+  from_input.next_in = (Bytef *) buffer;
+  from_input.avail_out = compressedBufferSize;
+  from_input.next_out = (Bytef *) compressedBuffer;
+
+  do {
+    int ret = deflate(&from_input, Z_SYNC_FLUSH);
+    if (ret == Z_STREAM_ERROR) {
+      fprintf(stderr, "Compressing stream state inconsistent: %s\n", from_input.msg);
+      exit(1);
+    }
+  } while (from_input.avail_in > 0);
+
+  return compressedBufferSize - from_input.avail_out;
+}
+
+int decompression(int bytes_to_decompress, char* buffer, char* decompressedBuffer, int decompressedBufferSize) {
+  from_server.avail_in = bytes_to_decompress;
+  from_server.next_in = (Bytef *) buffer;
+  from_server.avail_out = decompressedBufferSize;
+  from_server.next_out = (Bytef *) decompressedBuffer;
+
+  do {
+    int ret = inflate(&from_server, Z_SYNC_FLUSH);
+    if (ret == Z_STREAM_ERROR) {
+      fprintf(stderr, "Decompressing stream state inconsistent: %s\n", from_server.msg);
+      exit(1);
+    }
+  } while (from_server.avail_in > 0);
+
+  return decompressedBufferSize - from_server.avail_out;
+}
+
+void run() {
+  int ret;
+  struct pollfd fds[2];
+
+  fds[0].fd = 0; //standard input
+  fds[0].events = POLLIN | POLLHUP | POLLERR;
+
+  fds[1].fd = sockfd; //server output
+  fds[1].events = POLLIN | POLLHUP | POLLERR;
+
+  int size;
+  char buf[256];
+  char compressbuf[256];
+  char crlf[2] = {'\015', '\012'};
+  char lf[1] = {'\012'};
+  char temp;
+
+  while (1) {
+    ret = poll(fds, 2, 0);
+    if (ret == -1) {
+      fprintf(stderr, "Error while polling: %s\n", strerror(errno));
+      exit(1);
+    }
+
+    if (fds[0].revents & POLLIN) {
+      size = read(fds[0].fd, &buf, 256); //keyboard input
+      if (size < 0) {
+        fprintf(stderr, "Failed to read from keyboard input");
+        exit(1);
+      }
+      for(int i = 0; i < size; i++) {
+        temp = buf[i];
+        switch(temp) {
+          case '\012':
+          case '\015':
+            write(1, &crlf, 2);
+            if (!compress_flag)
+              write(sockfd, &lf, 1);
+            else
+              compressbuf[i] = lf[0];
+            break;
+          default:
+            write(1, &temp, 1);
+            if (!compress_flag)
+              write(sockfd, &temp, 1);
+            else
+              compressbuf[i] = temp;
+            break;
+        }
+        if (log_flag && !compress_flag) {
+          dprintf(logfile, "SENT %d bytes: ", 1);
+          write(logfile, &temp, 1);
+          write(logfile, &crlf[1], 1);
+        }
+      }
+      if (compress_flag) {
+        char tempbuf[256];
+        size = compression(size, compressbuf, tempbuf, 256);
+        write(sockfd, &tempbuf, size);
+        if (log_flag) {
+          dprintf(logfile, "SENT %d bytes: ", size);
+          write(logfile, &tempbuf, size);
+          write(logfile, &crlf[1], 1);
+        }
+      }
+
+      memset(buf, 0, size);
+    }
+
+    if (fds[1].revents & POLLIN) {
+      size = read(fds[1].fd, &buf, 256); //server output
+      if (size < 0) {
+        fprintf(stderr, "Failed to read server output");
+        exit(1);
+      }
+      if (log_flag) {
+        dprintf(logfile, "RECEIVED %d bytes: ", size);
+        write(logfile, &buf, size);
+        write(logfile, &crlf[1], 1);
+      }
+      if (compress_flag) {
+        char tempbuf[256];
+        size = decompression(size, buf, tempbuf, 256);
+        write(1, &tempbuf, size);
+      }
+      else {
+        write(1, &buf, size);
+      }
+      memset(buf, 0, size);
+    }
+
+    if ((POLLHUP | POLLERR) & fds[1].revents) {
+      exit(0);
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   //Process args
   int c;
-
+  int port_flag 0;
+  log_flag = 0;
+  compress_flag = 0;
   while(1) {
     static struct option long_options[] = {
         {"port",  required_argument,  0,  'p'},
+        {"log",   optional_argument,  0,  'l'},
+        {"compress",    optional_argument, 0,  'c'},
         {0, 0, 0, 0}
     };
     int option_index = 0;
@@ -99,26 +249,44 @@ int main(int argc, char **argv) {
     switch(c) {
       case 'p':
         port = atoi(optarg);
+        port_flag = 1;
+        break;
+      case 'l':
+        logfile = open(optarg, O_WRONLY | O_CREAT, 0666);
+        if (logfile < 0) {
+          fprintf(stderr, "Failed to create/open log file: %s\n", strerror(errno));
+          exit(1);
+        }
+        log_flag = 1;
+        break;
+      case 'c':
+        compress_flag = 1;
         break;
       default:
         fprintf(stderr, "Invalid arguments\n");
-        fprintf(stderr, "--port=port#  Port Number\tRequired\n");
-        fprintf(stderr, "--log=logfile Log File   \tOptional\n");
+        fprintf(stderr, "--port=port#  Port Number         Required\n");
+        fprintf(stderr, "--log=logfile Log File            Optional\n");
+        fprintf(stderr, "--compress    Enable Compression  Optional");
         exit(1);
     }
   }
+  if (!port_flag) {
+    fprintf(stderr, "Invalid arguments\n");
+    fprintf(stderr, "--port=port#      Port Number     Required\n");
+    fprintf(stderr, "--shell=<program> Shell program   Optional\n");
+    fprintf(stderr, "--compress    Enable Compression  Optional");
+    exit(1);
+  }
 
+  //Set terminal attributes
+  setTerminal();
+
+  //Connect to server
   connectToServer();
 
-  printf("Please enter the message: ");
-  bzero(buffer,256);
-  read(0, buffer, 10);
-  int n = write(sockfd,buffer,strlen(buffer));
-  if (n < 0)
-       printf("ERROR writing to socket");
-  bzero(buffer,256);
-  n = read(sockfd,buffer,255);
-  if (n < 0)
-       printf("ERROR reading from socket");
-  printf("%s\n",buffer);
+  if (compress_flag) {
+    setupCompression();
+  }
+
+  run();
 }
--- 1b/lab1b-server.c
+++ 1b/lab1b-server.c
@@ -4,7 +4,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <termios.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <errno.h>
@@ -15,11 +14,30 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <assert.h>
+#include "zlib.h"
 
 int port;
 int shell_flag;
+int compress_flag;
 char* exec_file;
+int pipeChild[2];
+int pipeParent[2];
+int pid;
 int newsockfd;
+z_stream from_shell;
+z_stream from_client;
+
+void signal_handler(int sigNum){
+    if(sigNum == SIGPIPE){
+        exit(0);
+    }
+    if(sigNum == SIGINT){
+        kill(pid, SIGINT);
+    }
+}
 
 void openSocket() {
   struct sockaddr_in serv_addr, cli_addr;
@@ -52,19 +70,256 @@ void openSocket() {
   //Succesfully connected to client
 }
 
+void shutdownServer() {
+  int status;
+  if (waitpid(pid, &status, 0) != pid) {
+    fprintf(stderr, "Waitpid error: %s\n", strerror(errno));
+    exit(1);
+  }
+  if (WIFEXITED(status)) {
+    fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\n", WTERMSIG(status), WEXITSTATUS(status));
+    exit(0);
+  }
+  exit(0);
+}
+
+void setupCompression() {
+  from_shell.zalloc = Z_NULL;
+  from_shell.zfree = Z_NULL;
+  from_shell.opaque = Z_NULL;
+  int ret = deflateInit(&from_shell, Z_DEFAULT_COMPRESSION);
+  if (ret < 0) {
+    fprintf(stderr, "Failed compression init\n");
+    exit(1);
+  }
+
+  from_client.zalloc = Z_NULL;
+  from_client.zfree = Z_NULL;
+  from_client.opaque = Z_NULL;
+  ret = inflateInit(&from_client);
+  if (ret < 0) {
+    fprintf(stderr, "Failed decompression init\n");
+    exit(1);
+  }
+}
+
+int compression(int bytes_to_compress, char* buffer, char* compressedBuffer, int compressedBufferSize) {
+  from_shell.avail_in = bytes_to_compress;
+  from_shell.next_in = (Bytef *) buffer;
+  from_shell.avail_out = compressedBufferSize;
+  from_shell.next_out = (Bytef *) compressedBuffer;
+
+  do {
+    int ret = deflate(&from_shell, Z_SYNC_FLUSH);
+    if (ret == Z_STREAM_ERROR) {
+      fprintf(stderr, "Compressing stream state inconsistent: %s\n", from_shell.msg);
+      exit(1);
+    }
+  } while (from_shell.avail_in > 0);
+
+  return compressedBufferSize - from_shell.avail_out;
+}
+
+int decompression(int bytes_to_decompress, char* buffer, char* decompressedBuffer, int decompressedBufferSize) {
+  from_client.avail_in = bytes_to_decompress;
+  from_client.next_in = (Bytef *) buffer;
+  from_client.avail_out = decompressedBufferSize;
+  from_client.next_out = (Bytef *) decompressedBuffer;
+
+  do {
+    int ret = inflate(&from_client, Z_SYNC_FLUSH);
+    if (ret == Z_STREAM_ERROR) {
+      fprintf(stderr, "Decompressing stream state inconsistent: %s\n", from_client.msg);
+      exit(1);
+    }
+  } while (from_client.avail_in > 0);
+
+  return decompressedBufferSize - from_client.avail_out;
+}
+
+
+void runChild() {
+  close(pipeChild[1]); //close child output
+  close(pipeParent[0]); //close input from parent
+
+  dup2(pipeChild[0], 0);
+  dup2(pipeParent[1], 1);
+
+  close(pipeChild[0]);
+  close(pipeParent[1]);
+
+  //Execute program
+  char* args[2] = {exec_file, NULL};
+  if (execvp(exec_file, args) == -1) {
+    fprintf(stderr, "Failed to execute program: %s\n", strerror(errno));
+    exit(1);
+  }
+}
+
+void runParent() {
+  close(pipeChild[0]); //close input from child
+  close(pipeParent[1]); //close parent output
+
+  struct pollfd fds[2];
+  int ret;
+
+  fds[0].fd = newsockfd; //client input
+  fds[0].events = POLLIN | POLLHUP | POLLERR;
+
+  fds[1].fd = pipeParent[0]; //shell output
+  fds[1].events = POLLIN | POLLHUP | POLLERR;
+
+  int size;
+  char buf[256];
+  char compressbuf[256];
+  char crlf[2] = {'\015', '\012'};
+  char lf[1] = {'\012'};
+  char temp;
+
+  while (1) {
+    ret = poll(fds, 2, 0);
+    if (ret == -1) {
+      fprintf(stderr, "Error while polling: %s\n", strerror(errno));
+    }
+
+    if (fds[0].revents & POLLIN) {
+      size = read(fds[0].fd, &buf, 256); //client input
+      if (size < 0) {
+        fprintf(stderr, "Failed to read from client");
+        exit(1);
+      }
+      if (compress_flag) {
+        char tempbuf[256];
+        size = decompression(size, buf, tempbuf, 256);
+        memcpy(buf, tempbuf, size);
+      }
+      for(int i = 0; i < size; i++) {
+        temp = buf[i];
+        switch(temp) {
+          case '\003':
+            kill(pid, SIGINT);
+            break;
+          case '\004':
+            close(pipeChild[1]);
+            break;
+          case '\012':
+          case '\015':
+            write(pipeChild[1], &lf, 1);
+            break;
+          default:
+            write(pipeChild[1], &buf, 1);
+            break;
+        }
+      }
+      memset(buf, 0, size);
+    }
+
+    if (fds[1].revents & POLLIN) { //shell output
+      size = read(pipeParent[0], &buf, 256);
+      if (size < 0) {
+        fprintf(stderr, "Failed to read from shell output");
+        exit(1);
+      }
+      int j = 0;
+      for(int i = 0; i < size; i++) {
+        temp = buf[i];
+        switch(temp) {
+          case '\012':
+            if (!compress_flag) {
+              write(newsockfd, &crlf, 2);
+            }
+            else {
+              compressbuf[j] = crlf[0];
+              j += 1;
+              compressbuf[j] = crlf[1];
+            }
+            break;
+          default:
+            if (!compress_flag) {
+              write(newsockfd, &temp, 1);
+            }
+            else
+              compressbuf[j] = temp;
+            break;
+        }
+        j++;
+      }
+      if (compress_flag) {
+        char tempbuf[256];
+        size = compression(j, compressbuf, tempbuf, 256);
+        write(newsockfd, &tempbuf, size);
+      }
+      memset(buf, 0, size);
+    }
+    if ((POLLHUP | POLLERR) & fds[1].revents) {
+      exit(0);
+    }
+  }
+}
+
+//Regular copy
+void copy() {
+  char buf[256];
+  char compressbuf[256];
+  char crlf[2] = {'\015', '\012'};
+  int size;
+  char temp;
+  while ((size = read(0, &buf, 256)) > 0) {
+    int j = 0;
+    for(int i = 0; i < size; i++) {
+      temp = buf[i];
+      switch(temp) {
+        case '\004':
+          exit(0);
+        case '\012':
+          if (!compress_flag) {
+            write(newsockfd, &crlf, 2);
+          }
+          else {
+            compressbuf[j] = crlf[0];
+            j += 1;
+            compressbuf[j] = crlf[1];
+          }
+          break;
+        default:
+          if (!compress_flag) {
+            write(newsockfd, &temp, 1);
+          }
+          else
+            compressbuf[j] = temp;
+          break;
+      }
+      j++;
+    }
+    if (compress_flag) {
+      char tempbuf[256];
+      size = compression(j, compressbuf, tempbuf, 256);
+      write(newsockfd, &tempbuf, size);
+    }
+    memset(buf, 0, size);
+  }
+}
+
 int main(int argc, char **argv) {
+  signal(SIGPIPE, signal_handler);
+  signal(SIGINT, signal_handler);
+  atexit(shutdownServer);
+
   //Process args
   int c;
+  int port_flag = 0;
+  compress_flag = 0;
   shell_flag = 0;
 
   while(1) {
     static struct option long_options[] = {
         {"port",  required_argument,  0,  'p'},
         {"shell",  optional_argument,  0,  's'},
+        {"compress",    no_argument,   0,  'c'},
         {0, 0, 0, 0}
     };
     int option_index = 0;
-    c = getopt_long(argc, argv, "s:", long_options, &option_index);
+    c = getopt_long(argc, argv, "p:s:", long_options, &option_index);
 
     if (c == -1)
       break;
@@ -72,27 +327,61 @@ int main(int argc, char **argv) {
     switch(c) {
       case 'p':
         port = atoi(optarg);
+        port_flag = 1;
         break;
       case 's':
         shell_flag = 1;
         exec_file = optarg;
         break;
+      case 'c':
+        compress_flag = 1;
+        break;
       default:
         fprintf(stderr, "Invalid arguments\n");
-        fprintf(stderr, "--port=port#      Port Number  \tRequired\n");
-        fprintf(stderr, "--shell=<program> Shell program\tOptional\n");
+        fprintf(stderr, "--port=port#      Port Number     Required\n");
+        fprintf(stderr, "--shell=<program> Shell program   Optional\n");
+        fprintf(stderr, "--compress    Enable Compression  Optional");
         exit(1);
     }
+  }
+  if (!port_flag) {
+    fprintf(stderr, "Invalid arguments\n");
+    fprintf(stderr, "--port=port#      Port Number     Required\n");
+    fprintf(stderr, "--shell=<program> Shell program   Optional\n");
+    fprintf(stderr, "--compress    Enable Compression  Optional");
+    exit(1);
   }
 
   openSocket();
 
-  char buffer[256];
-  int size;
-  bzero(buffer, 256);
-
-  while ((size = read(newsockfd, &buffer, 255)) > 0) {
-    printf("got message: %s\n", buffer);
-    write(newsockfd,"I got your message",18);
+  if (compress_flag) {
+    setupCompression();
   }
+
+  if (shell_flag) {
+    //Create pipes and fork
+    if (pipe(pipeChild) != 0) {
+      fprintf(stderr, "Error creating child pipe: %s\n", strerror(errno));
+      exit(1);
+    }
+    if (pipe(pipeParent) != 0) {
+      fprintf(stderr, "Error creating parent pipe: %s\n", strerror(errno));
+      exit(1);
+    }
+
+    pid = fork();
+    if (pid < -1) {
+      fprintf(stderr, "Error while forking: %s\n", strerror(errno));
+      exit(1);
+    }
+    else if (pid == 0) {
+      runChild();
+    }
+    else {
+      runParent();
+    }
+  }
+
+  copy();
+  exit(0);
 }
